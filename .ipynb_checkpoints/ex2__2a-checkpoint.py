0e210aab4ef14da75e0b29532ecfd25ae176680d
Jessica <jtetteh@ictp.it>
2024-01-19 11:11:21 +0200

    test

--- .ipynb_checkpoints/ex2__2a-checkpoint.py
+++ .ipynb_checkpoints/ex2__2a-checkpoint.py
@@ -3,14 +3,14 @@ import ex2__2
 
 if __name__ == "__main__":
     start_time = time.time()
-    print(ex2__2.recaman(100))
+    ex2__2.recaman(100000)
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Elapsed time: {elapsed_time} seconds")
 
 
     start_time = time.time()
-    ex2__2.recaman2(100)
+    ex2__2.recaman2(100000)
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"Elapsed time: {elapsed_time} seconds")
---
+++ ex4_.py
@@ -0,0 +1,10 @@
+import functools
+
+def decor(array)
+     @functools.wraps(func)
+    def wrapper(array):
+        if np.array is empty:
+            return print("array is empty")
+        else:
+            return func(array)
+    return wrapper
